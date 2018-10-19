2fa80ed7fbb857973bb4eb3c0533bd834e2776c1
Nick <nick_stolk9@hotmail.com>
2018-10-19 12:03:45 +0200

    Small additions to guest page and script

--- insert guest/submitGuest.js
+++ insert guest/submitGuest.js
@@ -4,19 +4,21 @@ let id = 1;
 
 function GuestData(){
   // de value's van de input fields
+  document.getElementById('arrayOfGuest').innerHTML = "";
   let guestName = document.getElementById("guestNameId").value;
   let address = document.getElementById("addressId").value;
   let homeTown = document.getElementById("homeTownId").value;
   let postalCode = document.getElementById("postalCodeId").value;
-  let paspoortnummer = document.getElementById("paspoortnummer").value;
+  let telephonenumber = document.getElementById("telephonenumber").value;
+  let emailaddress = document.getElementById("emailaddress").value;
+  let passportnumber = document.getElementById("passportnumber").value;
   let paymentMethod = document.getElementById("paymentMethodId").value;
-  let guest = new Guest(id, guestName, address, homeTown, postalCode, paspoortnummer, paymentMethod);
+  let guest = new Guest(id, guestName, address, homeTown, postalCode,
+    telephonenumber, emailaddress, passportnumber, paymentMethod);
   arr.push(guest);
   console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    document.getElementById('arrayOfGuest').innerHTML = `${arr[i].guestName} <br>
-    ${arr[i].address} <br> ${arr[i].homeTown} <br> ${arr[i].postalCode} <br>
-    ${arr[i].guestID} <br> ${arr[i].paymentMethod}`;
-  }
+  document.getElementById('arrayOfGuest').innerHTML = `Name: ${guestName} <br> Address: ${address} <br>
+  Home Town: ${homeTown} <br> Postal Code: ${postalCode} <br> Telephone Number: ${telephonenumber} <br>
+  E-mail Address: ${emailaddress} <br> Passport Number: ${passportnumber} <br> Payment Method: ${paymentMethod}`;
   id += 1;
 }
