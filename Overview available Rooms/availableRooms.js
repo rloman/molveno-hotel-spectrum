005b2fd4fc4caf771169ee0c35a3b0f6a38109f1
Gijs Repriels <g.repriels@gmail.com>
2018-10-26 16:13:42 +0200

    update

--- Overview available Rooms/availableRooms.js
+++ Overview available Rooms/availableRooms.js
@@ -1,1 +1,8 @@
 "use strict"
+
+function inputDate() {
+  let checkIn = document.getElementById('checkIn').value;
+  let checkOut = document.getElementById('checkOut').value;
+  console.log(checkIn);
+  console.log(checkOut);
+}
