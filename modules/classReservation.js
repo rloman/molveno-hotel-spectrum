fa1e719c4399dc55a3ffdcdd194626a56f38f1fa
Lemeza <nick_stolk9@hotmail.com>
2018-10-30 11:05:00 +0100

    Add room reservation

--- modules/classReservation.js
+++ modules/classReservation.js
@@ -1,20 +1,38 @@
-"use strict"
+"use strict";
 
-class classReservation {
-  constructor(id, roomNumber, checkInDate, checkOutDate, guestName, guestTelephoneNumber, guestEmailAddress) {
+class Reservation {
+  constructor(id, guestFirstName, guestLastName, roomNumber, guestTelephoneNumber,
+    guestEmailAddress, numberOfGuests, checkInDate, checkOutDate) {
     this.id = id;
+    this.guestFirstName = guestFirstName;
+    this.guestLastName = guestLastName;
     this.roomNumber = roomNumber;
-    this.checkInDate = checkInDate;
-    this.checkOutDate = checkOutDate;
-    this.guestName = guestName;
     this.guestTelephoneNumber = guestTelephoneNumber;
     this.guestEmailAddress = guestEmailAddress;
+    this.numberOfGuests = numberOfGuests;
+    this.checkInDate = checkInDate;
+    this.checkOutDate = checkOutDate;
   }
   get id() {
     return this._id;
   }
+  get guestFirstName() {
+    return this._guestFirstName;
+  }
+  get guestLastName() {
+    return this._guestLastName;
+  }
   get roomNumber() {
     return this._roomNumber;
+  }
+  get guestTelephoneNumber() {
+    return this._guestTelephoneNumber;
+  }
+  get guestEmailAddress() {
+    return this._guestEmailAddress;
+  }
+  get numberOfGuests() {
+    return this._numberOfGuests;
   }
   get checkInDate() {
     return this._checkInDate;
@@ -22,35 +40,32 @@ class classReservation {
   get checkOutDate() {
     return this._checkOutDate;
   }
-  get guestName() {
-    return this._guestName;
-  }
-  get guestTelephoneNumber() {
-    return this._guestTelephoneNumber;
-  }
-  get guestEmailAddress() {
-    return this._guestEmailAddress
-  }
 
   set id(value) {
     this._id = value;
   }
+  set guestFirstName(value) {
+    this._guestFirstName = value;
+  }
+  set guestLastName(value) {
+    this._guestLastName = value;
+  }
   set roomNumber(value) {
     this._roomNumber = value;
-  }
-  set checkInDate(value) {
-    this._checkInDate = value;
-  }
-  set checkOutDate(value) {
-    this._checkOutDate = value;
-  }
-  set guestName(value) {
-    this._guestName = value;
   }
   set guestTelephoneNumber(value) {
     this._guestTelephoneNumber = value;
   }
   set guestEmailAddress(value) {
     this._guestEmailAddress = value;
+  }
+  set numberOfGuests(value) {
+    this._numberOfGuests = value;
+  }
+  set checkInDate(value) {
+    this._checkInDate = value;
+  }
+  set checkOutDate(value) {
+    this._checkOutDate = value;
   }
 }
---
+++ room_reservation/reserveRoom.js
@@ -0,0 +1,22 @@
+"use strict";
+let arr = [];
+let id = 0;
+
+const roomReservation = () => {
+  let guestFirstName = document.getElementById("firstName").value;
+  let guestLastName = document.getElementById("lastName").value;
+  let telephonenumber = document.getElementById("telephonenumber").value;
+  let emailaddress = document.getElementById("emailaddress").value;
+  let people = document.querySelector('input[name="people"]:checked').value;
+  let roomNumber = document.getElementById("roomNumber").value;
+  let checkInDate = document.getElementById("arrivalDate").value;
+  let checkOutDate = document.getElementById("depatureDate").value;
+  if (checkInDate > checkOutDate) {
+    throw new Error("Arrival date can't be later than departure date");
+  }
+  let reservation = new Reservation(id, guestFirstName, guestLastName, roomNumber, telephonenumber,
+  emailaddress, people, checkInDate, checkOutDate);
+  arr.push(reservation);
+  console.log(arr);
+  id += 1;
+}
