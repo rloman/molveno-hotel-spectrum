1a61cd37dc600651d4ab0a5824c0a287c40d4704
ron <rontim82@gmail.com>
2018-11-08 15:16:57 +0100

    add changed overview available rooms

--- Overview available Rooms/availableRooms.js
+++ Overview available Rooms/availableRooms.js
@@ -1,8 +1,39 @@
-"use strict";
+//"use strict";
 
 function inputDate() {
   let checkIn = document.getElementById('checkIn').value;
   let checkOut = document.getElementById('checkOut').value;
-  let roomType = document.querySelector("input[name='roomType']:checked").value;
-  let numberBeds = document.getElementById('optionNumberBeds').value;
+  console.log(checkIn);
+  console.log(checkOut);
+  let output = document.getElementById("vacantRooms");
+
+//console.log("hotelRooms: " + hotelRooms)
+  for (i = 0; i < hotelRooms.length; i++) {
+      if(hotelRooms[i].checkOutDate <= checkIn || hotelRooms[i].checkInDate >= checkOut) {
+        console.log((hotelRooms[i]));
+        output.innerHTML = `Room: ${hotelRooms[i].roomNumber}`;
+      }
+  }
+
+
 }
+/*
+/*function checkOverlap(){
+
+/*for (i = 0; i < hotelRooms.length; i++) {
+    if(hotelRooms[i].checkInDate <= formInput.checkOutDate && formInput.checkInDate <= hotelRooms[i].checkOutDate) {
+      console.log((hotelRooms[i]));
+      //document.write(cars[i].make + ", " + cars[i].model);
+    }
+}
+}
+
+
+for (i = 0; i < hotelRooms.length; i++) {
+    if(hotelRooms[i].checkInDate <= formInput.checkOutDate && formInput.checkInDate <= hotelRooms[i].checkOutDate) {
+      console.log((hotelRooms[i]));
+      //document.write(cars[i].make + ", " + cars[i].model);
+    }
+}
+}
+*/
