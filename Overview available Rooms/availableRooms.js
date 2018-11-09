2290ffc13dad1fe1b1f1a5bc43b0a6efb7742ce6
ron <rontim82@gmail.com>
2018-11-09 15:19:42 +0100

    add past date restriction, number beds filtered

--- Overview available Rooms/availableRooms.js
+++ Overview available Rooms/availableRooms.js
@@ -1,46 +1,67 @@
 //"use strict";
-
+//let now = new Date();
+//let today = new Date();
 function inputDate() {
-  let checkIn = document.getElementById('checkIn').value;
-  let checkOut = document.getElementById('checkOut').value;
+  let checkIn = document.getElementById('arrivalDate').value;
+  let checkOut = document.getElementById('departureDate').value;
   console.log(checkIn);
   console.log(checkOut);
   let output = document.getElementById("vacantRooms");
   let roomType = document.querySelector('input[name="room"]:checked').value;
-  output.innerHTML = `<b>These are the ${roomType} rooms: </b><br>`;
+  let numberOfBeds = document.querySelector('input[name="beds"]:checked').value;
+  output.innerHTML = `<b>These are the ${roomType} rooms available: </b><br>`;
 
   //console.log("hotelRooms: " + hotelRooms)
   for (i = 0; i < hotelRooms.length; i++) {
     if (hotelRooms[i].checkOutDate <= checkIn || hotelRooms[i].checkInDate >= checkOut) {
-console.log("roomType:" + roomType);
+      console.log("roomType:" + roomType);
       if (roomType === hotelRooms[i].roomType) {
-
-        //console.log((hotelRooms[i]));
-        output.innerHTML += `Room: ${hotelRooms[i].roomNumber} <br>`;
+        if (numberOfBeds <= hotelRooms[i].numberOfPeople) {
+          //console.log((hotelRooms[i]));
+          output.innerHTML += `Room: ${hotelRooms[i].roomNumber} <br>`;
+        }
       }
-
     }
   }
-
-
 }
+
+function today() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; //January is 0!
+  let yyyy = today.getFullYear();
+   if(dd<10){
+          dd='0'+dd
+      }
+      if(mm<10){
+          mm='0'+mm
+      }
+
+  today = yyyy+'-'+mm+'-'+dd;
+  document.getElementById("arrivalDate").setAttribute("min", today);
+  document.getElementById("departureDate").setAttribute("min", today);
+};
+
 /*
-/*function checkOverlap(){
-
-/*for (i = 0; i < hotelRooms.length; i++) {
-    if(hotelRooms[i].checkInDate <= formInput.checkOutDate && formInput.checkInDate <= hotelRooms[i].checkOutDate) {
-      console.log((hotelRooms[i]));
-      //document.write(cars[i].make + ", " + cars[i].model);
-    }
-}
-}
-
-
-for (i = 0; i < hotelRooms.length; i++) {
-    if(hotelRooms[i].checkInDate <= formInput.checkOutDate && formInput.checkInDate <= hotelRooms[i].checkOutDate) {
-      console.log((hotelRooms[i]));
-      //document.write(cars[i].make + ", " + cars[i].model);
-    }
-}
+function todayF() {
+  today.setDate(now.getDate() + 1);
+  function day_of_the_month(d)
+  {
+    return (d.getDate() < 10 ? '0' : '') + d.getDate();
+  }
+  function month_of_the_year(d)
+  {
+    return ((d.getMonth() + 1) < 10 ? '0' : '') + (d.getMonth() + 1);
+  }
+  let dateControl = document.querySelector('input[name="arrivalDate"]');
+  dateControl.setAttribute("min", `${now.getFullYear()}-${month_of_the_year(now)}-${day_of_the_month(now)}`);
+  dateControl.value = now.getFullYear() + "-" + month_of_the_year(now) + "-" + day_of_the_month(now);
+  dateControl = document.querySelector('input[name="departureDate"]');
+  dateControl.setAttribute("min", `${now.getFullYear()}-${month_of_the_year(today)}-${day_of_the_month(today)}`);
+  dateControl.value = now.getFullYear() + "-" + month_of_the_year(today) + "-" + day_of_the_month(today);
 }
 */
+
+function cancel() {
+  location.reload(true);
+}
