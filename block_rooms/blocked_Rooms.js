d84c88573db8761f42650b7883b6cad79f49b28d
ron <rontim82@gmail.com>
2018-11-14 03:02:55 +0100

    add function secondDateAfterFirst

--- block_rooms/blocked_Rooms.js
+++ block_rooms/blocked_Rooms.js
@@ -1,9 +1,8 @@
 "use strict";
-//let now = new Date();
-//let today = new Date();
 
-function blockRoom(){
-
+function secondDateAfterFirst() {
+  let firstDate = document.getElementById("arrivalDate").value;
+  let secondDate = document.getElementById("departureDate").setAttribute("min", firstDate);
 }
 
 function inputDate() {
@@ -48,39 +47,20 @@ function submitBlock() {
 function today() {
   let today = new Date();
   let dd = today.getDate();
-  let mm = today.getMonth()+1; //January is 0!
+  let mm = today.getMonth() + 1; //January is 0!
   let yyyy = today.getFullYear();
-   if(dd<10){
-          dd='0'+dd
-      }
-      if(mm<10){
-          mm='0'+mm
-      }
+  if (dd < 10) {
+    dd = '0' + dd
+  }
+  if (mm < 10) {
+    mm = '0' + mm
+  }
 
-  today = yyyy+'-'+mm+'-'+dd;
+  today = yyyy + '-' + mm + '-' + dd;
   document.getElementById("arrivalDate").setAttribute("min", today);
   document.getElementById("departureDate").setAttribute("min", today);
 };
 
-/*
-function todayF() {
-  today.setDate(now.getDate() + 1);
-  function day_of_the_month(d)
-  {
-    return (d.getDate() < 10 ? '0' : '') + d.getDate();
-  }
-  function month_of_the_year(d)
-  {
-    return ((d.getMonth() + 1) < 10 ? '0' : '') + (d.getMonth() + 1);
-  }
-  let dateControl = document.querySelector('input[name="arrivalDate"]');
-  dateControl.setAttribute("min", `${now.getFullYear()}-${month_of_the_year(now)}-${day_of_the_month(now)}`);
-  dateControl.value = now.getFullYear() + "-" + month_of_the_year(now) + "-" + day_of_the_month(now);
-  dateControl = document.querySelector('input[name="departureDate"]');
-  dateControl.setAttribute("min", `${now.getFullYear()}-${month_of_the_year(today)}-${day_of_the_month(today)}`);
-  dateControl.value = now.getFullYear() + "-" + month_of_the_year(today) + "-" + day_of_the_month(today);
-}
-*/
 
 function cancel() {
   location.reload(true);
