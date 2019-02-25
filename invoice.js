af566282fca8e9a692f1d94117c14f58174c3531
Lemeza <nick_stolk9@hotmail.com>
2019-02-25 21:22:40 +0100

    Invoices should work properly now

--- invoice.js
+++ invoice.js
@@ -1,5 +1,6 @@
 "use strict";
 const apiURL = "http://api.test.hotelmolveno.carpago.nl/api";
+// const apiURL = "http://localhost:8081/api";
 // full URL: http://api.test.hotelmolveno.carpago.nl/api/reservationjoinguestonid/1
 // Joins on reservation ID
 function invoice(reservationID) {
@@ -8,35 +9,49 @@ function invoice(reservationID) {
     type: "get",
     dataType: "json",
     success: function(reservationData) {
-      let arrival = new Date(reservationData[0].arrivalDate);
-      let departure = new Date(reservationData[0].departureDate);
-      let total = totalDays(reservationData[0].arrivalDate, reservationData[0].departureDate);
-      let totalPrice = reservationData[0].roomPrice * total;
-      $("#invoiceModal").html(`<div class="modal-content"><div class="modal-header">
-        <h2>Invoice of ${reservationData[0].firstName} ${reservationData[0].lastName}.</h2></div>
-        <div class="modal-body"><div class="row"><div class="col"><b>Stayed in room:</b> ${reservationData[0].roomNumber} (${reservationData[0].roomType})</div></div>
-        <div class="row"><div class="col"><b>Arrival date:</b> ${arrival.getDate()}-${arrival.getMonth()+1}-${arrival.getFullYear()}</div>
-        <div class="col"><b>Departure date:</b> ${departure.getDate()}-${departure.getMonth()+1}-${departure.getFullYear()}</div></div><br />
-        <div class="row"><div class="col"><b>Total days:</b> ${total} (€ ${reservationData[0].roomPrice},- per day)</div><div class="col">€ ${totalPrice},-</div></div><br />
-        <div id='accessories'><b>Accessories</b></div><br /><div class="row" id="totalPrice"></div></div></div>`);
-        $.ajax({
-          url: `${apiURL}/accessoriesofreservation/${reservationID}`,
-          type: "get",
-          dataType: "json",
-          success: function(accessoryData) {
-            let totalAcc = 0;
-            for (let i = 0; accessoryData.length > i; i++) {
-              $("#accessories").append(`<div class="row"><div class="col">${accessoryData[i].name} (€ ${accessoryData[i].price} per day)</div><div class="col">€ ${accessoryData[i].price * total},-</div></div>`);
-              totalAcc += (accessoryData[i].price * total);
-            }
-            $("#accessories").append(`<div class="row"><div class="col"><b>Total Price Accessories:</b></div><div class="col">€ ${totalAcc},-</div></div>`);
-            $("#totalPrice").html(`<div class="col"><b>Total:</b></div><div class="col"><b><u>€ ${totalPrice + totalAcc},-</u></b></div>`);
-            return;
-          },
-          error: function(error) {
-            console.log(error);
-          }
-        });
+      let arrival = moment(reservationData.arrivalDate).format('DD-MM-YYYY');
+      let departure = moment(reservationData.departureDate).format('DD-MM-YYYY');
+      let total = totalDays(reservationData.arrivalDate, reservationData.departureDate);
+      console.log(reservationData.numberofGuests);
+      let vat = reservationData.numberofGuests * 2 * total; // VAT is €2 per guest per night stayed at the hotel
+      let totalPrice = reservationData.roomPrice * total + vat;
+      $("#invoiceModal").html(`
+        <div class="modal-content">
+        <div class="modal-header">
+        <h2>Invoice of ${reservationData.firstName} ${reservationData.lastName}</h2>
+        </div>
+        <div class="modal-body">
+        <div class="row">
+        <div class="col">
+        <b>Stayed in room:</b> ${reservationData.roomNumber} (${reservationData.roomType})
+        </div>
+        </div>
+        <div class="row">
+        <div class="col">
+        <b>Arrival date:</b> ${arrival}
+        </div>
+        <div class="col">
+        <b>Departure date:</b> ${departure}
+        </div>
+        </div>
+        <br />
+        <div class="row">
+        <div class="col">
+        <b>Total days:</b> ${total} (€ ${reservationData.roomPrice},- per day)
+        </div>
+        <div class="col">
+        <b>Room cost:</b> € ${total * reservationData.roomPrice},-
+        <br>
+        <b>Value-added Tax:</b> € ${vat},-
+        </div>
+        </div>
+        <br />
+        <div class="row">
+        <div class="col">
+        </div>
+        <div class="col"><b>Total:</b><b> <u>€ ${totalPrice},-</u></b></div>
+        </div>
+        `);
       return;
     },
     error: function(error) {
@@ -50,5 +65,3 @@ function totalDays(ar, dep) {
   let departure = new Date(dep);
   return ((((departure.getTime() - arrival.getTime()) / 1000) / 60) / 60) / 24;
 }
-
-invoice(2);
