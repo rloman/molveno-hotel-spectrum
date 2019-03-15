1b49e569f037aa7a51885dd5f6a7e44b57298f59
Lemeza <nick_stolk9@hotmail.com>
2019-03-15 13:11:49 +0100

    Styling for login and register

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
  'connection':{
   'host':'localhost',
-  'user':'ron',
-  'password':'ron2018!'
+  'user':'nick',
+  'password':'nick2018!'
  },
  'database':'nodejs_login',
  'user_table':'users'
